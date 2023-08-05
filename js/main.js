f7d40f6f8fca1ff21aacaac938d17ebc196aab43
MendyBrotzky <141170071+MendyBrotzky@users.noreply.github.com>
2023-08-05 20:12:31 +0300

    a little change

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
 const EMPTY = ' '
 const MINE = '💣'
 const FLAGE = '🚩'
+const LOSE_FACE = '😪'
+const WIN_FACE = '😎'
+var gTimerInterval
+
 var gBoard
 var gNegsCount = 0
 
@@ -12,7 +16,7 @@ var gLevel = {
 }
 
 var gGame = {
-  isOn: false,
+  isOn: true,
   shownCount: 0,
   markedCount: 0,
   secsPassed: 0,
@@ -22,11 +26,14 @@ var gGame = {
 
 
 function onInIt() {
-
+  clearInterval(gTimerInterval)
+  gGame.isOn = true
+  gGame.markedCount = 0
+  gGame.shownCount = 0
   gBoard = buildBoard()
+  setMinesRandomly(gBoard)
   countNegsAroundCell(gBoard)
   renderBoard(gBoard)
-
 }
 
 
@@ -47,7 +54,9 @@ function buildBoard(levelSize) {
       }
     }
 
-  }              ///    rando mines bring back       /////
+  }
+
+  ///    rando mines bring back       /////
   /////////////////////////////////////////////////////////////////////
   // for(var k = 0 ; k < gLevel.MINES; k++){                      /////
   //   var currEmptyCell = findEmptyCells()                       /////
@@ -56,15 +65,16 @@ function buildBoard(levelSize) {
   // }                                                           //////
   ////////////////////////////////////////////////////////////////////////
 
-  board[0][1].type = MINE
-  board[0][1].isMine = true
-  board[1][3].type = MINE
-  board[1][3].isMine = true
+  // board[0][1].type = MINE
+  // board[0][1].isMine = true
+  // board[1][3].type = MINE
+  // board[1][3].isMine = true
   return board
 }
 
 
 function renderBoard() {
+
   var elBoard = document.querySelector('.board')
 
   var strHTML = ''
@@ -111,15 +121,67 @@ function countNegsAroundCell(board) {
   }
 }
 
-function onCellClicked(elCell, i, j) {
-  var allCellsWithMine = document.querySelector(`.cell-${i}-${j}`)
 
-  var currCell = gBoard[i][j]
-  if (currCell.isMarked) {
+function onCellClicked(elCell, i, j) {
+  if (gGame.isOn === false) {
     return
   }
 
-  if (currCell.type === EMPTY && currCell.minesAroundCount === 0) {
+  gTimerInterval = setInterval(timer)
+  checkGameOver(gBoard)
+  // if (gGame.isOn === false) {
+  //   // console.log(i, j);
+  //   // console.log(gBoard[i][j])
+  //   gGame.isOn = true
+  //   // gBoard[i][j].type = EMPTY
+  //   // elCellsWithMine.innerText = gBoard[i][j].type
+  //   // elCellsWithMine.style.backgroundColor = 'blue'
+  //   return
+  // }
+
+
+  elCellsWithMine = document.querySelector(`.cell-${i}-${j}`)
+  var elCellsWithMine = document.querySelector(`.cell-${i}-${j}`)
+  var currCell = gBoard[i][j]
+  if (currCell.isShown || currCell.isMarked) {
+
+    return
+  }
+
+
+  if (currCell.type === MINE) {
+    var elResetBtn = document.querySelector('.reset')
+    elResetBtn.innerText = LOSE_FACE
+    clearInterval(gTimerInterval)
+    console.log('Game is over!');
+    // checkGameOver()
+    gGame.isOn = false
+    elCell.innerText = currCell.type
+    currCell.isShown = true
+    for (var i = 0; i < gLevel.SIZE; i++) {
+      for (var j = 0; j < gLevel.SIZE; j++) {
+        if (gBoard[i][j].type === MINE) {
+          elCellsWithMine = document.querySelector(`.cell-${i}-${j}`)
+          elCellsWithMine.innerText = currCell.type
+        }
+      }
+    }
+
+  } else if (currCell.minesAroundCount > 0) {
+    elCell.innerText = currCell.minesAroundCount
+    currCell.isShown = true
+
+
+
+  } else {
+    elCell.style.backgroundColor = 'blue'
+    currCell.isShown = true
+
+  }
+
+  if (currCell.type === EMPTY && currCell.minesAroundCount === 0 /* && !currCell.isShown */) {
+    currCell.isShown = true
+
 
     var rowIdx = i
     var colIdx = j
@@ -129,96 +191,78 @@ function onCellClicked(elCell, i, j) {
       for (var n = colIdx - 1; n <= colIdx + 1; n++) {
         if (n < 0 || n >= gLevel.SIZE) continue
         if (m === rowIdx && n === colIdx) continue
-        // console.log(gBoard[n][m].location);
-        // currCell = gBoard[m][n]
 
-        // expandShown(gBoard, elCell, i, j)
 
-        var nRowIdx = gBoard[m][n].location.i
-        var nColIdx = gBoard[m][n].location.j
-        console.log(nRowIdx , nColIdx);
+        var elNewCell = document.querySelector(`.cell-${m}-${n}`)
+        if (gBoard[m][n].minesAroundCount > 0) {
+          elNewCell.innerText = gBoard[m][n].minesAroundCount
+          gBoard[m][n].isShown = true
 
-        for (var k = nRowIdx - 1; k <= nRowIdx + 1; k++) {
-          if (k < 0 || k >= gLevel.SIZE) continue
-          for (var l = nColIdx - 1; l <= nColIdx + 1; l++) {
-            if (l < 0 || l >= gLevel.SIZE) continue
-            if (k === nRowIdx && l === nColIdx) continue
-            console.log(gBoard[k][l].location);
-            var elNewCell = document.querySelector(`.cell-${k}-${l}`)
-            console.log(elNewCell);
-            elNewCell.innerText = gBoard[k][l].minesAroundCount
-            
-            
-          }
+
+        } else {
+          elNewCell.style.backgroundColor = 'blue'
+          gBoard[m][n].isShown = true
+
         }
+        // var nRowIdx = gBoard[m][n].location.i
+        // var nColIdx = gBoard[m][n].location.j
+
+        // for (var k = nRowIdx - 1; k <= nRowIdx + 1; k++) {
+        //   if (k < 0 || k >= gLevel.SIZE) continue
+        //   for (var l = nColIdx - 1; l <= nColIdx + 1; l++) {
+        //     if (l < 0 || l >= gLevel.SIZE) continue
+        //     if (k === nRowIdx && l === nColIdx) continue
+        //   }
+        // }
       }
     }
-  }
-
-
-  if (currCell.type === MINE) {
-    elCell.innerText = currCell.type
-    currCell.isShown = true
-    for (var i = 0; i < gLevel.SIZE; i++) {
-      for (var j = 0; j < gLevel.SIZE; j++) {
-        if (gBoard[i][j].type === MINE) {
-          allCellsWithMine = document.querySelector(`.cell-${i}-${j}`)
-          allCellsWithMine.innerText = currCell.type
-        }
-      }
-    }
-
-  } else if (currCell.minesAroundCount > 0) {
-    elCell.innerText = currCell.minesAroundCount
-    currCell.isShown = true
-  } else {
-    elCell.style.backgroundColor = 'blue'
-    currCell.isShown = true
   }
 }
 
 
-
-
 function addFlage(elCell, i, j) {
+  if (gGame.isOn === false) {
+    return
+  }
 
   var currCell = gBoard[i][j]
 
   if (!currCell.isMarked && !currCell.isShown) {
     elCell.innerText = FLAGE
     currCell.isMarked = true
+    gGame.markedCount++
+    console.log('gGame.markedCount:', gGame.markedCount);
   } else if (currCell.isMarked) {
     elCell.innerText = EMPTY
     currCell.isMarked = false
+    gGame.markedCount--
+    console.log(gGame.markedCount);
   }
 }
 
 
-// function expandShown(board, elCell, i, j) {
-//   var minesCount = 0
-// for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
-//       if (i < 0 || i >= gLevel.SIZE) continue
-//       for (var j = colIdx - 1; j <= colIdx + 1; j++) {
-//         if (j < 0 || j >= gLevel.SIZE) continue
-//         if (i === rowIdx && j === colIdx) continue
-//         if (board[i][j].type === MINE) {
-//           minesCount++
-//         }
-//       }
-//     }
-//     board[rowIdx][colIdx].minesAroundCount = minesCount
-//     minesCount
-
-//     elCell.innerText = gBoard[i][j].minesAroundCount
-  
-// }
-
-function checkGameOver() {
+function checkGameOver(board) {
+  if (gGame.markedCount === gLevel.MINES &&
+    (gGame.shownCount === gLevel.SIZE ** 2) - gLevel.MINES) {
+    gGame.isOn = false
+    var elResetBtn = document.querySelector('.reset')
+    elResetBtn.innerText = WIN_FACE
+    console.log('you Won!')
+  }
 }
 
 function getClassName(location) {
   const cellClass = 'cell-' + location.i + '-' + location.j
   return cellClass
+}
+
+function setMinesRandomly(board) {
+  for (var k = 0; k < gLevel.MINES; k++) {
+    var currEmptyCell = findEmptyCells()
+    board[currEmptyCell[0].i][currEmptyCell[0].j].type = MINE
+  }
+
+
 }
 
 function getRandomInt(min, max) {
@@ -250,4 +294,14 @@ function findEmptyCells() {
   return allTheCells
 }
 
-// function()
+function timer() {
+  var startTime = Date.now()
+
+  gTimerInterval = setInterval(() => {
+    var elapsedTime = Date.now() - startTime
+    document.querySelector('.clock').innerText = (
+      elapsedTime / 1000
+    ).toFixed(3)
+  }, 37)
+}
+
